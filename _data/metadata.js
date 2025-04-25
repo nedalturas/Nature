d79c1584c4e00dbb10503d1748e579ea64a2964a
httpwanderer <nedalturas527@gmail.com>
2025-04-26 01:27:07 +0800

    Filesetup and initial commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Nature",
+	url: "https://example.com/",
+	language: "en",
+	description: "A personal reflection based on the nature of things",
+	author: {
+		name: "Your Name Here",
+		email: "youremailaddress@example.com",
+		url: "https://example.com/about-me/"
+	}
+}
